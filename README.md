fa40e0174dccce77bf6606c1ec13563f9cded11e
julien-cpsn <julien.caposiena@gmail.com>
2023-08-21 22:32:06 +0200

    Added optional arg and updated README

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 Now Playing SERATO
 ===
 
-This is a little script to display the currently playing track on the Serato DJ software.
+[![Rust](https://github.com/Julien-cpsn/NowPlayingSerato/actions/workflows/rust.yml/badge.svg)](https://github.com/Julien-cpsn/NowPlayingSerato/actions/workflows/rust.yml)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+This is a little script to display the currently playing tracks of the Serato DJ software.
+
+## How to use
 
 You can run the script using :
 ```bash
@@ -13,4 +18,12 @@ OR
 ```bash
 cargo build
 ./target/debug/now-playing-serato "C:\\Users\\<User>\\Music\\_Serato_"
+```
+
+## Optional arguments
+
+You can add `-t` or `--tracks` to change the number of tracks to display. The default is 1.
+
+```shell
+cargo run -- "C:\\Users\\<User>\\Music\\_Serato_" --tracks 5
 ```
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,19 @@ use std::io::Read;
 use std::path::{PathBuf};
 use std::thread::sleep;
 use std::time::{Duration, SystemTime};
-use colored::Colorize;
+use colored::{Colorize};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// `_Serato_` folder path
+    serato_path: String,
+
+    /// Number of last played track to display
+    #[arg(short, long, default_value_t = 1)]
+    tracks: usize,
+}
 
 struct Track {
     title: String,
@@ -25,7 +37,7 @@ impl Debug for Track {
 
 impl Display for Track {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", format!(" {:70}\n {:70}", &self.title.bold(), &self.artist).on_truecolor(0, 255, 0))
+        write!(f, "{}", format!("{:99.99}\n {:101.101}", self.title.bold(), self.artist).on_truecolor(0, 255, 0))
     }
 }
 
@@ -36,9 +48,12 @@ const SONG_END_BYTES: &str = "\x00\x00\x00\x00\x0f";
 const BYTES_TO_SKIP: usize = 10;
 
 fn main() {
-    let _serato_ = std::env::args().nth(1).expect("No _serato_ path given");
+    let args = Args::parse();
 
-    let paths = fs::read_dir(_serato_ + "\\History\\Sessions\\").expect("Cannot open folder");
+    let number_of_last_tracks = args.tracks;
+    let serato_path = PathBuf::from(args.serato_path);
+
+    let paths = fs::read_dir(serato_path.join("History").join("Sessions")).expect("Cannot open _serato_ folder");
 
     let mut max_date = SystemTime::UNIX_EPOCH;
     let mut path_with_date = PathBuf::new();
@@ -67,7 +82,7 @@ fn main() {
         let stringed_content = latin1_to_string(content.as_slice());
         let raw_tracks = stringed_content.split("oent")
             .map(|data| {
-                data.replace(|c: char| !c.is_ascii(), "*")
+                data.replace(|c: char| !c.is_ascii(), "*").replace("\n", "")
             })
             .collect::<Vec<String>>();
 
@@ -98,10 +113,32 @@ fn main() {
         }
 
 
-        if let Some(last_track) = tracks.last() {
-            print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-            println!("{}", last_track);
-        };
+        let tracks_len = tracks.len();
+        let tracks_to_display: usize;
+
+        if number_of_last_tracks < tracks_len {
+            tracks_to_display = number_of_last_tracks
+        }
+        else {
+            tracks_to_display = tracks_len;
+        }
+
+        print!("\x1B[2J\x1B[1;1H");
+        //print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
+        for (index, track) in tracks[tracks_len-tracks_to_display..tracks_len].iter().enumerate() {
+            println!("{:100}", "".on_truecolor(0, 255, 0));
+            print!("{}", " ".on_truecolor(0, 255, 0));
+
+            if index == number_of_last_tracks-1 {
+                print!("{}", "► ".bold().on_truecolor(0, 255, 0));
+                println!("{}", track.to_string().blink());
+            }
+            else {
+                println!("{}", track.to_string());
+            }
+        }
+        println!("{:100}", "".on_truecolor(0, 255, 0));
 
         //println!("{:?}", tracks);
 
